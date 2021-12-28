07fd4d9dde8b45e3e00c78c4ada7ecf7f023bbac
Emad <sudani.emad@gmail.com>
2021-12-28 22:19:32 +0330

    init

---
+++ src/main/java/com/favacircle/Managingdevices/ManagingDevicesApplication.java
@@ -0,0 +1,13 @@
+package com.favacircle.Managingdevices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManagingDevicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ManagingDevicesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/favacircle/Managingdevices/ManagingDevicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.favacircle.Managingdevices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ManagingDevicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
